01ea67e7c8eb303d438651ef984082bb7f40471f
ryann254 <keddelyronjoz@gmail.com>
2020-07-18 15:59:40 +0300

    [feature:readmore-functionality]:Adding the read more functionality
    -Fixed the functionality on 768px and 992px view
    -Added js to for the above views

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,4 @@
-//Getting the elements
+//Getting the elements for the read more functionality
 const readPeacan = document.querySelector('.read-pecan')
 const readJourney = document.querySelector('.read-journey')
 const readItalian = document.querySelector('.read-italian')
@@ -14,6 +14,14 @@ const contentItalian = document.querySelector('#italian')
 const contentLemon = document.querySelector('#lemon')
 const imageInner = document.querySelectorAll('.image-inner')
 const backButton = document.querySelectorAll('.back')
+const pecanContainer = document.querySelector('#pecan-container')
+const journeyContainer = document.querySelector('#journey-container')
+const italianContainer = document.querySelector('#italian-container')
+const lemonContainer = document.querySelector('#lemon-container')
+const imageHeader = document.querySelectorAll('#heading')
+const navigator = document.querySelector('.page-navigator')
+const bgImage = document.querySelectorAll('.bg-image')
+const footer = document.querySelector('footer')
 
 let names = [readPeacan,readJourney,readItalian,readLemon]
 for(n=0; n<names.length; n++){
@@ -21,7 +29,11 @@ for(n=0; n<names.length; n++){
     backButton[n].addEventListener('click', toggleItem)
 }
 
+//Toggles the read more section
 function toggleItem(e) {
+    moveOtherRecipes(e)
+    navigator.classList.toggle('hide-navigator')
+    footer.classList.toggle('move-footer')
     if(e.target.className === 'read-pecan' || e.target.id === 'back-pecan') {
         hiddenContentPecan.classList.toggle('show-content')
         contentPecan.classList.toggle('hide-inner-content')
@@ -41,5 +53,55 @@ function toggleItem(e) {
         hiddenContentLemon.classList.toggle('show-content')
         contentLemon.classList.toggle('hide-inner-content')
         imageInner[3].classList.toggle('hide-inner-image')
+    }
+}
+
+//moves the other recipes on click of the read link
+function moveOtherRecipes(e) {
+    if(e.target.className === 'read-pecan' || e.target.id === 'back-pecan') {
+        journeyContainer.classList.toggle('move-recipes')
+        italianContainer.classList.toggle('move-recipes-1')
+        lemonContainer.classList.toggle('move-recipes-2')
+        for(n=1; n<imageInner.length; n++){
+            imageInner[n].classList.toggle('move-image-inner')
+            imageHeader[n].classList.toggle('move-image-header')
+            bgImage[n].classList.toggle('move-bg-image')
+        }
+    }
+    else if(e.target.className === 'read-journey' || e.target.id === 'back-journey') {
+        pecanContainer.classList.toggle('move-recipes')
+        italianContainer.classList.toggle('move-recipes-1')
+        lemonContainer.classList.toggle('move-recipes-2')
+        imageInner[0].classList.toggle('move-image-inner')
+        imageHeader[0].classList.toggle('move-image-header')
+        bgImage[0].classList.toggle('move-bg-image')
+        for(n=2; n<imageInner.length; n++){
+            imageInner[n].classList.toggle('move-image-inner')
+            imageHeader[n].classList.toggle('move-image-header')
+            bgImage[n].classList.toggle('move-bg-image')
+        }
+    }
+    else if(e.target.className === 'read-italian' || e.target.id === 'back-italian') {
+        pecanContainer.classList.toggle('move-recipes')
+        journeyContainer.classList.toggle('move-recipes-1')
+        lemonContainer.classList.toggle('move-recipes-2')
+        imageInner[3].classList.toggle('move-image-inner')
+        imageHeader[3].classList.toggle('move-image-header')
+        bgImage[3].classList.toggle('move-bg-image')
+        for(n=0; n<imageInner.length-2; n++){
+            imageInner[n].classList.toggle('move-image-inner')
+            imageHeader[n].classList.toggle('move-image-header')
+            bgImage[n].classList.toggle('move-bg-image')
+        }
+    }
+    else {
+        pecanContainer.classList.toggle('move-recipes')
+        journeyContainer.classList.toggle('move-recipes-1')
+        italianContainer.classList.toggle('move-recipes-2')
+        for(n=0; n<imageInner.length-1; n++){
+            imageInner[n].classList.toggle('move-image-inner')
+            imageHeader[n].classList.toggle('move-image-header')
+            bgImage[n].classList.toggle('move-bg-image')
+        }
     }
 }
